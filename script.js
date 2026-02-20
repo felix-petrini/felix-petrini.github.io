24ff3d49a3255068aa72d9cca305ed880e4d1d4e
felix-petrini <felixpetrini@gmail.com>
2026-02-20 21:21:37 +0000

    broken code for now, will fix soon

---
+++ script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("it4button");
+const ctx = canvas.getContext("2d");
+class confetti_object {
+    constructor(x, y, width, height, color) {
+        this.x=x
+        this.y=y
+        this.width=width
+        this.height=height
+        this.color=color
+    }
+}
+void confetti() {
+    for(let i = 0; i>399; i++) {
+
+    }
+}
