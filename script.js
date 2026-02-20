d3737274c787f4fa2f133c9f009a2678eea929fb
felix-petrini <felixpetrini@gmail.com>
2026-02-20 22:59:43 +0000

    fixed script.js

--- script.js
+++ script.js
@@ -19,21 +19,32 @@ let currently_confettiing = false;
 function confetti() {
     if(currently_confettiing === false) {
         currently_confettiing = true;
-        for(let i = 0; i<401; i++) {
-            confettis.push(confetti_object(0, 0, 10, 20, (colors[colors.length % i]), Math.floor(Math.random() * max_xchange) - 10, Math.floor(Math.random() * max_ychange) - 10));
+        for(let i = 0; i < 100; i++) {
+            confettis.push(new confetti_object(200, 200, 10, 10, colors[i % colors.length], Math.random() * 10 - 5, Math.random() * -10));
         }
-        while(confettis.length > 0) {
-            let c = 0
-            for(const obj in confettis) {
-                obj.x = obj.x + obj.xchange;
-                obj.y = obj.y + obj.ychange;
-                obj.ychange = obj.ychange + 1;
-                ctx.fillStyle = obj.color;
-                ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
-                if(obj.y < 0 || obj.y > 400 || obj.x < 0 || obj.x > 400) {
-                    confettis.splice(c, 1);
-                }
-            }
+        update();
+    }
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = confettis.length - 1; i >= 0; i--) {
+        let obj = confettis[i];
+        obj.x += obj.xchange;
+        obj.y += obj.ychange;
+        obj.ychange += 1;
+
+        ctx.fillStyle = obj.color;
+        ctx.fillRect(obj.x, obj.y, obj.width, obj.height);
+
+        if(obj.y > 400 || obj.x < 0 || obj.x > 400) {
+            confettis.splice(i, 1);
         }
+    }
+
+    if (confettis.length > 0) {
+        requestAnimationFrame(update);
+    } else {
+        currently_confettiing = false;
     }
 }
