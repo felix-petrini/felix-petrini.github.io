61e9e2a15712ecf5e53bcb3847e3bd8154f25ac7
felix-petrini <felixpetrini@gmail.com>
2026-02-25 21:50:21 +0000

    fix please

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function update2() {
     ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
     if (!shop && !titlescreen) {
         ctx2.fillText(score.toString(), score_x + (15 * (score.toString().length-1)), score_y);
-        ctx2.strokeRect(canvas2.width - 225, 75, 150, 50);
+        ctx2.strokeRect(canvas2.width/2 + 135, 75, canvas2.height/2 - 190, 50);
         ctx2.fillText("Shop", canvas2.width - 150, 75);
         if (clicked) {
             ctx2.drawImage(button_img, 0, 178.8, 178.8, 178.8, (canvas2.width-178.8)/2, (canvas2.height-178.8)/2, 178.8, 178.8);
